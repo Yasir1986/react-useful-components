055d9294bdfe4eab2f6a7734887df505aff6578e
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-05-31 17:33:13 +0300

    Date comp added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home.jsx";
 import Slider from "./components/Slider.jsx";
 import { BrowserRouter as Router, Route  } from "react-router-dom";
 import GoogleMap from "./components/GoogleMap.jsx";
+import Date from "./components/Date";
 import './App.css';
 
 
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/slider" component={Slider} />
         <Route path="/map" component={GoogleMap} />
+        <Route path="/date" component={Date} />
 
     </Router>
   );
---
+++ src/components/Date.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+import Burgermenu from "./Burgermenu.jsx";
+import styled from "styled-components";
+
+
+
+ class Date extends React.Component {
+  render() {
+    return (
+      <Wrapper>
+        <Burgermenu />
+         <h2>Date Picker</h2>
+           <p>Please type a day:</p>
+      <DayPickerInput onDayChange={day => console.log(day)} />
+     </Wrapper>
+    );
+  }
+}
+
+
+export default Date;
+
+const Wrapper = styled.div`
+    text-align: center;
+`;
+
