aece5885aeb29f2bcdcfb70a166b31227ff25aaa
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-05-31 17:43:57 +0300

    date updated

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -14,6 +14,7 @@ import styled from "styled-components";
          <h2>Date Picker</h2>
            <p>Please type a day:</p>
       <DayPickerInput onDayChange={day => console.log(day)} />
+    
      </Wrapper>
     );
   }
